4ce819425ca46689c44c77cadf4828a3e46b5682
cristina1084 <cristina.johns1084@gmail.com>
2019-07-30 11:53:40 +0530

    read json data

--- html/js/ajaxdemo.js
+++ html/js/ajaxdemo.js
@@ -2,7 +2,11 @@ $(document).ready(function(){             //jQuery
     $("#btn1").click(function(){
         $.ajax({                        //AJAX format
             type:"GET",                 //type mentions the method to be used 
-            url :"mydata.json"          
+            url :"mydata.json",
+            success : function(data){
+                console.log(data);
+                
+            }         
         });
     });
 });
