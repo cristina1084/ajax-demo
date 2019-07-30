b6ddb18f6888d2e44879454b50c3a274c816d143
cristina1084 <cristina.johns1084@gmail.com>
2019-07-30 12:29:09 +0530

    display json data

--- html/js/ajaxdemo.js
+++ html/js/ajaxdemo.js
@@ -4,8 +4,16 @@ $(document).ready(function(){             //jQuery
             type:"GET",                 //type mentions the method to be used 
             url :"mydata.json",
             success : function(data){
-                console.log(data);
-                
+                //console.log(data);
+                var table = "<table class='table table-bordered'> <thead> <tr> <th> NAME </th> <th> DOB </th> <th> COLLEGE </th> <th> MOBILE </th> </thead>";
+                for (var i in data){  //Array traversal
+                    table+= "<tr> <td>" + data[i].name + "</td>";
+                    table+= "<td>" + data[i].dob + "</td>";
+                    table+= "<td>" + data[i].college + "</td>";
+                    table+= "<td>" + data[i].mobile + "</td> </tr>";
+                }
+                table+="</table>";
+                $(".results").html(table);   
             }         
         });
     });
